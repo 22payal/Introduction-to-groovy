f36c3e5b22b9f8cc600baba4085e301741968c42
22payal <payalnigam1996@gmail.com>
2018-03-22 10:39:36 +0530

    new commit

--- src/main/groovy/Question5.groovy
+++ src/main/groovy/Question5.groovy
@@ -35,13 +35,15 @@ class Question5
         if(list) { println "test evaluated to true inside if \n" }
 
         println("observation 9 :")
-        HourMinute hourMinute = new HourMinute()
-        hourMinute.hour=12
-        hourMinute.minute=45
+        
+        HourMinute obj1= new HourMinute(9,58)
+        HourMinute obj2= new HourMinute(1,20)
 
-        println("when overloading plus operator -> "+hourMinute.hour.plus(hourMinute.minute))
+        HourMinute obj3= obj1+obj2
+        println("addition"+obj3)
 
-        println("when overloading minus operator -> "+hourMinute.hour.minus(hourMinute.minute))
+        HourMinute obj4= obj1-obj2
+        println("substraction"+obj4)
 
     }
 }
@@ -50,4 +52,41 @@ class HourMinute
 {
     int hour
     int minute
+
+    HourMinute(int hour, int minute) {
+        this.hour = hour
+        this.minute = minute
+    }
+
+    HourMinute plus(HourMinute a){
+        this.minute+=a.minute;
+        this.hour+=a.hour
+        if(this.minute>=60){
+            this.minute=this.minute%60;
+            this.hour=(this.hour+1)%24;
+        }
+        return this;
+    }
+
+    HourMinute minus(HourMinute a){
+        this.minute-=a.minute;
+        this.hour-=a.hour
+        if(this.minute<0){
+            this.minute=60+this.minute;
+            this.hour--
+            if(this.hour<0){
+                this.hour=23;
+            }
+        }
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "HourMinute{" +
+                "hour=" + hour +
+                ", minute=" + minute +
+                '}';
+    }
 }
